40657b7a72166b70450ec97bc2e3cdf13a3fb5fe
LUCIANO KOHLER DA SILVA <luciano.silva2@alunos.sc.senac.br>
2023-08-17 15:39:59 -0300

    progresso no circulo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 console.log('Hello, world!')
+
+let habs = document.getElementsByClassName('element');
+let midTitle = document.querySelector('#middle #title')
+let midDesc = document.querySelector('#middle #desc')
+
+for(let i = 0; i< habs.length; i++){
+    habs[i].addEventListener('mouseover', () =>{
+        for(let j = 0; j< habs.length; j++){
+            habs[j].style.animationPlayState = 'paused';
+        }
+        changeDesc(habInfos[i][0])
+    })
+
+    habs[i].addEventListener('mouseout', () =>{
+        for(let j = 0; j< habs.length; j++){
+            habs[j].style.animationPlayState = 'running';
+        }
+        changeDesc('Minhas habilidades', 'Passe o mouse em cima das minhas capacitações para saber mais sobre')
+    })
+}
+
+function changeDesc(newTitle, newDesc){
+    midTitle.style.color = 'white';
+    
+    setTimeout(() =>{
+        midTitle.innerHTML = newTitle
+    }, 200)
+
+    setTimeout(() =>{
+        midTitle.style.color = 'black';
+    }, 400)
+}
+
+let habInfos = [
+  //[nome, desc]
+    ["HTML",""],
+    ["JavaScript",""], 
+    ["TypeScript",""], 
+    ["C++",""], 
+    ["Canva",""], 
+    ["Figma",""]
+];
